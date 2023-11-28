4a6d5cc3a076f4bd0d58f2a1ca4ddc7a437e21f9
xypp3 <a@b.com>
2023-11-28 00:30:38 +0000

    feat: Benchmark task 1

---
+++ test-task1.py
@@ -0,0 +1,23 @@
+from matplotlib import pyplot as plt
+import requests
+import time
+
+
+num_of_sensors = [1, 10, 50, 100, 500, 1000]  #, 5000, 10000]
+response_times = [-1.0 for i in range(len(num_of_sensors))]
+
+url = input("Function url: ")
+
+for i in range(len(num_of_sensors)):
+    start = time.perf_counter()
+    requests.get(url + f"&num={num_of_sensors[i]}")
+    end = time.perf_counter()
+    response_times[i] = end - start
+
+    print(f"Benchark on {num_of_sensors[i]} done! and it took {response_times[i] :0.3f} seconds")
+
+plt.title("Benchmark for Task 1")
+plt.xlabel("Number of sensors")
+plt.ylabel("Response time in (seconds)")  # TODO: this unit
+plt.plot(num_of_sensors, response_times)
+plt.show()
