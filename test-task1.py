d21e6064a9c7f9d15b0a1662a0ccf5b734802710
xypp3 <a@b.com>
2023-11-28 01:05:04 +0000

    fix: Save graph before displaying

--- test-task1.py
+++ test-task1.py
@@ -1,9 +1,9 @@
 from matplotlib import pyplot as plt
 import requests
 import time
+import os
 
-
-num_of_sensors = [1, 10, 50, 100, 500, 1000]  #, 5000, 10000]
+num_of_sensors = [1, 10, 50, 100]  #, 500, 1000]  #, 5000, 10000]
 response_times = [-1.0 for i in range(len(num_of_sensors))]
 
 url = input("Function url: ")
@@ -20,4 +20,9 @@ plt.title("Benchmark for Task 1")
 plt.xlabel("Number of sensors")
 plt.ylabel("Response time in (seconds)")  # TODO: this unit
 plt.plot(num_of_sensors, response_times)
+
+# save before displaying
+my_path = os.path.dirname(os.path.abspath(__file__))
+plt.savefig(os.path.join(my_path, "benchmark.png"))
+
 plt.show()
